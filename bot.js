ba9fa1c47f8aa55813b99f87d938d8ea87f9db64
malken21 <Malken_Marumasa@outlook.jp>
2022-09-05 10:24:24 +0900

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,14 +1,21 @@
-const { Client, Intents } = require("discord.js");
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ], partials: [Partials.Channel]
+});
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
-client.on('ready', () => {
-  console.log(`login!!\n${client.user.tag}`);
+client.on("ready", () => {
+    console.log(`login: (${client.user.tag})`);
 });
 
 client.on('messageCreate', message => {
-	if (message.content === 'test' && !message.author.bot) {
-	    message.reply({ content: 'test', allowedMentions: { repliedUser: false }})
-	}
+    if (message.content === 'test' && !message.author.bot) {
+        message.reply({ content: 'test', allowedMentions: { repliedUser: false } })
+    }
 });
-client.login('ここに自分のbotのTOKENをいれて');
+
+client.login("ここに自分のbotのTOKENをいれて");
